3e128fc2f953084568e2f903359899f59bbdce9d
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-09-12 12:46:23 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,5 @@
+import objectFitImages from 'object-fit-images';
+objectFitImages();
 import './import/ui';
 import './import/swiper';
-import objectFitImages from 'object-fit-images';
-// import Stickyfill from 'stickyfilljs';
 
-
-
-objectFitImages();
-// var elements = $('.aside');
-// const sticky = new Stickyfill.Sticky(element);
